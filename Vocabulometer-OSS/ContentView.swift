5e6a9e6470ebb2b064191a4edd26f40e64129cd3
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-03 11:41:42 +0900

    Initial Commit

---
+++ Vocabulometer-OSS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Vocabulometer-OSS
+//
+//  Created by iwashita on 2022/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vocabulometer-OSS/Vocabulometer_OSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocabulometer_OSSApp.swift
+//  Vocabulometer-OSS
+//
+//  Created by iwashita on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct Vocabulometer_OSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
