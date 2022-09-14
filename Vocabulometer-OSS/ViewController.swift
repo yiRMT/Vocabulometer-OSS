64d525f4c3f868ec677b8454d665cdc0abaefb7f
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-14 15:32:10 +0900

    Replaced to UIKit (#7)

---
+++ Vocabulometer-OSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vocabulometer-OSS
+//
+//  Created by iwashita on 2022/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
