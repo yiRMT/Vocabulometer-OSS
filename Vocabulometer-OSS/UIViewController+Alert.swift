31d587e76c1555ed925a7bf471df9400dc9b7464
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-24 03:47:02 +0900

    [add] Alert menu
    - added alert menu as an extension for UIViewController

---
+++ Vocabulometer-OSS/UIViewController+Alert.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Alert.swift
+//  Vocabulometer-OSS
+//
+//  Created by iwashita on 2022/09/24.
+//
+
+import UIKit
+
+public extension UIViewController {
+    // MARK: Public Methods
+    
+    func showAlert(title: String, message: String, actions: [UIAlertAction]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions.forEach { alert.addAction($0) }
+        present(alert, animated: true, completion: nil)
+    }
+}
